1919dd9cac315ee4f05a3380791c7ae91b700cb6
tomoyuki-nakabayashi <tomo.wait.for.it.yuki@gmail.com>
2018-10-30 21:50:39 +0900

    rust-lld: error: undefined symbol: abort...

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,18 @@
 #![no_std]
 #![no_main]
 
+extern crate rlibc;
+
 global_asm!(include_str!("boot.s"));
 
 #[no_mangle]
 pub extern "C" fn __start_rust(x: i32, y: i32) -> i32 {
     x + y
+}
+
+use core::panic::PanicInfo;
+#[panic_handler]
+#[no_mangle]
+pub fn panic(_info: &PanicInfo) -> ! {
+    loop{}
 }
