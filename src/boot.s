b823c5c7d9a82bb93f8501b701350ab32f16a376
tomoyuki-nakabayashi <tomo.wait.for.it.yuki@gmail.com>
2018-10-30 21:08:33 +0900

    first commit.

---
+++ src/boot.s
@@ -0,0 +1,9 @@
+.global _start
+
+_start:
+    addi    x1, zero, 0
+
+    /* Now jump to the rust world; __start_rust.  */
+    auipc   t0, __start_rust
+    addi    t0, t0, __start_rust
+    jr      t0
---
+++ src/lib.rs
@@ -0,0 +1,9 @@
+#![feature(global_asm)]
+#![no_std]
+
+global_asm!(include_str!("boot.s"));
+
+#[no_mangle]
+pub extern "C" fn __start_rust(x: i32, y: i32) -> i32 {
+    x + y
+}
