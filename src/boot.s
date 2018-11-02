2a016ba687532c6d41883baf0d9d1fec7fc8a36c
tomoyuki-nakabayashi <tomo.wait.for.it.yuki@gmail.com>
2018-11-02 16:35:36 +0900

    feature: Implementing uart16550 output.

--- src/boot.s
+++ src/boot.s
@@ -6,6 +6,10 @@
 _start:
     addi    x1, zero, 0
 
+    /* Set up stack pointer. */
+    auipc   sp, %hi(stacks + 1024)
+    addi    sp, sp, %lo(stacks + 1024)
+
     /* Now jump to the rust world; __start_rust.  */
     auipc   t0, %hi(__start_rust)
     addi    t0, t0, %lo(__start_rust)
@@ -14,3 +18,7 @@ _start:
 abort:
 
 .bss
+
+    .global stacks
+stacks:
+    .skip 1024
--- src/lib.rs
+++ src/lib.rs
@@ -5,6 +5,11 @@ global_asm!(include_str!("boot.s"));
 
 #[no_mangle]
 pub fn __start_rust(x: i32, y: i32) -> i32 {
+    let uart_16550 = 0x1000_0000 as *mut u8;
+    unsafe {
+        *uart_16550 = 'a' as u8;
+    }
+
     x + y
 }
 
